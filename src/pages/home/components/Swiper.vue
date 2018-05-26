ad85e3ffc02ed66e2af86c153c1b6a3ba298c8cd
yulixin <a139053012@qq.com>
2018-05-26 14:56:46 +0800

    添加轮播

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wraper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item in swiperList">
+        <img class="swiper-img" :src="item.imgUrl" alt="">
+      </swiper-slide>
+
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name:'HomeSwiper',
+  data:function () {
+    return{
+      swiperOption:{
+        pagination:'.swiper-pagination',
+        loop:true
+      },
+      swiperList:[
+        {
+        id:0,
+        imgUrl:'http://img1.qunarzz.com/piao/fusion/1805/e5/59fad13a64807d02.jpg_750x200_713ae984.jpg'
+      },
+        {
+          id:1,
+          imgUrl:'http://img1.qunarzz.com/piao/fusion/1805/77/f63bd04dd5319602.jpg_750x200_6ba8e0ca.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.wraper >>> .swiper-pagination-bullet-active
+  background-color #fff
+  .wraper
+    overflow hidden
+    width: 100%
+    height:0
+    padding-bottom:31.25%
+    .swiper-img
+      width: 100%
+</style>
