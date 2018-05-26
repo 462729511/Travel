5e4212b0e2e3a2247693bb65924ebb91324ff87b
yulixin <a139053012@qq.com>
2018-05-26 13:41:31 +0800

    project init
    Signed-off-by: yulixin <a139053012@qq.com>

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -6,10 +6,12 @@
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
     name:'home',
     components:{
-      HomeHeader
+      HomeHeader,
+      HomeSwiper
     }
 }
 </script>
