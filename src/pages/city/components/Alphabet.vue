60a75c8251f07754ea23bdacdc1ad89b62c63f7d
yulixin <a139053012@qq.com>
2018-05-27 21:03:53 +0800

    city-list css finish

---
+++ src/pages/city/components/Alphabet.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul class="list" ref="wrapper">
+   <li class="item">A</li>
+   <li class="item">A</li>
+   <li class="item">A</li>
+   <li class="item">A</li>
+   <li class="item">A</li>
+   <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name:'CityAlphabet',
+
+  }
+</script>
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .list
+    position: absolute;
+    top:1.58rem
+    right:0
+    bottom:0
+    width:.4rem
+    display: flex
+    justify-content center
+    flex-direction column
+    .item
+      line-height: .4rem
+      text-align: center
+      color: $bgColor
+</style>
